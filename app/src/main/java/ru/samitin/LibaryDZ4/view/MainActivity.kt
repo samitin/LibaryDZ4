77c1abefb9dce0c786d3cc518cb16e4d3c97eeed
samitin <samitin.e@gmail.com>
2021-11-19 11:26:04 +1100

    начало работы

---
+++ app/src/main/java/ru/samitin/LibaryDZ4/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package ru.samitin.LibaryDZ4.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.samitin.LibaryDZ4.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
